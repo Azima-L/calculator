ac2e78b322c0f73e92fca35404ee2a7a9494caeb
Azima Leyyan <azimaleyyan4@gmail.com>
2026-06-15 03:34:03 +0300

    New "Geometry Calculator" feature
    Refined the last updated code and introduced a new feature for geometry calculation—I did the UI for the logic to be integrated.

--- calculator.py
+++ calculator.py
@@ -1,67 +1,193 @@
 import math
+
+
 def print_divider():
       """Prints a consistent visual seperator."""
       print(f"\n{'-' * 50}\n")
 
 print_divider()
-print("Welcome to Our Calculator!\n"
+
+print("WELCOME TO OUR CALCULATOR!\n"
       "\n"
-      "1 - Add\n"
-      "2 - Subtract\n"
-      "3 - Multiply\n"
-      "4 - Divide\n"
-      "5 - power\n"
-      "6 - square root\n"
+      "1 - Standard Calculator\n"
+      "2 - Geometry Calculator\n"
       "\n"
       "0 - Exit\n"
       )
 
 while True:
-      operator = input("Choose an operator (0-6): ")
+      calculator = input("Choose a Calculator (0-2): ")
       print_divider()
 
-      if operator in ['0', '1', '2', '3', '4', '5', '6']:
+      if calculator in ['0', '1', '2']:
             break
       else:
-            print("Invalid! Please enter a number between 1 to 4.\n")
+            print("Invalid! Please enter a number between 0 to 2.\n")
 
-
-if operator == '0':
+if calculator == '0':
       print("Exiting...")
       print_divider()
       exit()
-elif operator == '1':
-      print("select two number (a + b)")
-      a = float(input("input value of a: "))
-      b = float(input("input value of b: "))
-      value = a + b
-      print(f"your answer is {value:.2f}")
-elif operator == '2':
-      print("select two number (a + b)")
-      a = float(input("input value of a: "))
-      b = float(input("input value of b: "))
-      value = a - b
-      print(f"your answer is {value:.2f}")
-elif operator == '3':
-      print("select two number (a * b)")
-      a = float(input("input value of a: "))
-      b = float(input("input value of b: "))
-      value = a * b
-      print(f"your answer is {value:.2f}")
-elif operator == '4':
-      print("select two number (a / b)")
-      a = float(input("input value of a: "))
-      b = float(input("input value of b: "))
-      value = a / b
-      print(f"your answer is {value:.2f}")
-elif operator == '5':
-      print("select two number (a^b)")
-      a = float(input("input value of a: "))
-      b = float(input("input value of b: "))
-      value = a ** b
-      print(f"your answer is {value:.2f}")
-elif operator == '6':
-      print("select a number (√a)")
-      a = float(input("input value of a: "))
-      value = math.sqrt(a)
-      print(f"your answer is {value:.2f}")
+
+
+# STANDARD CALCULATOR
+
+elif calculator == "1":
+      print("[Standard Calculator]\n"
+            "\n"
+            "1 - Add\n"
+            "2 - Subtract\n"
+            "3 - Multiply\n"
+            "4 - Divide\n"
+            "\n"
+            "5 - Power\n"
+            "6 - Square Root\n"
+            )
+
+      while True:
+            operator = input("Choose an operator (1-6): ")
+            print_divider()
+
+            if operator in ['1', '2', '3', '4', '5', '6']:
+                  break
+            else:
+                  print("Invalid! Please enter a number between 1 to 6.\n")
+
+      if operator == '1':
+            print("Input two numbers (a + b)\n")
+            a = float(input("Input the value of a: "))
+            b = float(input("Input the value of b: "))
+            value = a + b
+            print_divider()
+            print(f"Your answer is {value:.2f}")
+            print_divider()
+      elif operator == '2':
+            print("Input two numbers (a - b)\n")
+            a = float(input("Input the value of a: "))
+            b = float(input("Input the value of b: "))
+            value = a - b
+            print_divider()
+            print(f"Your answer is {value:.2f}")
+            print_divider()
+      elif operator == '3':
+            print("Input two numbers (a * b)\n")
+            a = float(input("Input the value of a: "))
+            b = float(input("Input the value of b: "))
+            value = a * b
+            print_divider()
+            print(f"Your answer is {value:.2f}")
+            print_divider()
+      elif operator == '4':
+            print("Input two numbers (a / b)\n")
+            a = float(input("Input the value of a: "))
+            b = float(input("Input the value of b: "))
+            value = a / b
+            print_divider()
+            print(f"Your answer is {value:.2f}")
+            print_divider()
+      elif operator == '5':
+            print("Input two numbers (a^b)\n")
+            a = float(input("Input the value of a: "))
+            b = float(input("Input the value of b: "))
+            value = a ** b
+            print_divider()
+            print(f"Your answer is {value:.2f}")
+            print_divider()
+      elif operator == '6':
+            print("Input a number (√a)\n")
+            a = float(input("Input the value of a: "))
+            value = math.sqrt(a)
+            print_divider()
+            print(f"Your answer is {value:.2f}")
+            print_divider()
+
+
+# GEOMETRY CALCULATOR
+
+elif calculator == "2":
+      print("[Geometry Calculator]\n"
+            "\n"
+            "1 - Rectangle\n"
+            "2 - Triangle\n"
+            "3 - Circle\n"
+            )
+      
+      while True:
+            shape = input("Choose a shape (1-3): ")
+            print_divider()
+
+            if shape in ['1', '2', '3']:
+                  break
+            else:
+                  print("Invalid! Please enter a number between 1 to 3.\n")
+      
+      if shape == '1':
+            print("[Geometry Calculator / Rectangle]\n"
+                  "\n"
+                  "1 - Area\n"
+                  "2 - Perimeter\n"
+                  "3 - Diagonal Length\n"
+                  )
+            
+            while True:
+                  operation = input("Choose an operation (1-3): ")
+                  print_divider()
+
+                  if operation in ['1', '2', '3']:
+                        break
+                  else:
+                        print("Invalid! Please enter a number between 1 to 3.\n")
+
+            if operation == '1':
+                  pass
+            elif operation == '2':
+                  pass
+            elif operation == '3':
+                  pass
+
+      elif shape == '2':
+            print("[Geometry Calculator / Triangle]\n"
+                  "\n"
+                  "1 - Area\n"
+                  "2 - Perimeter\n"
+                  "3 - Angles\n"
+                  )
+            while True:
+                  operation = input("Choose an operation (1-3): ")
+                  print_divider()
+
+                  if operation in ['1', '2', '3']:
+                        break
+                  else:
+                        print("Invalid! Please enter a number between 1 to 3.\n")
+
+            if operation == '1':
+                  pass
+            elif operation == '2':
+                  pass
+            elif operation == '3':
+                  pass
+
+      elif shape == '3':
+            print("[Geometry Calculator / Circle]\n"
+                  "\n"
+                  "1 - Area\n"
+                  "2 - Circumference\n"
+                  "3 - Diameter\n"
+                  )
+            
+            while True:
+                  operation = input("Choose an operation (1-3): ")
+                  print_divider()
+
+                  if operation in ['1', '2', '3']:
+                        break
+                  else:
+                        print("Invalid! Please enter a number between 1 to 3.\n")
+
+            if operation == '1':
+                  pass
+            elif operation == '2':
+                  pass
+            elif operation == '3':
+                  pass
